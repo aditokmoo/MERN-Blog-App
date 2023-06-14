b3c8396ab7544b3099c9267113b2a91d3e5ca96d
Havock48 <adit.github@gmail.com>
2023-06-15 00:09:19 +0200

    validation for profile edit finished

--- frontend/src/pages/auth/UserDetails.jsx
+++ frontend/src/pages/auth/UserDetails.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useUser } from '../../hooks/useUser';
 import axios from 'axios';
+// messsage modal
+import { ToastContainer, toast } from 'react-toastify';
 // react icons
 import { FaRegSave, FaEdit } from 'react-icons/fa';
 // Images
@@ -26,7 +28,9 @@ export const UserDetails = () => {
 	const [ fileImage, setFileImage ] = useState();
 	const [ file, setFile ] = useState();
 	const [ edit, setEdit ] = useState(false);
+	const [ isDisabled, setIsDisabled ] = useState(true);
 
+	// Handle user details change
 	const handleFormChange = (e) => {
 		// Set user details form data
 		setProfileData((prevState) => ({
@@ -35,13 +39,27 @@ export const UserDetails = () => {
 		}));
 	};
 
+	// Enable user edit
+	const editUserData = () => {
+		if (!profileData.username && !profileData.email) {
+			setIsDisabled(true);
+		} else {
+			setIsDisabled(false);
+		}
+
+		setEdit((prevState) => !prevState);
+	};
+
+	// Handle image change
 	const handleImageChange = (e) => {
 		setFile(e.target.files[0]);
 		const reader = new FileReader();
 		reader.readAsDataURL(e.target.files[0]);
 		reader.onload = () => setFileImage(reader.result);
+		setIsDisabled(false);
 	};
 
+	// Update user
 	const updateProfile = async (e) => {
 		e.preventDefault();
 
@@ -58,69 +76,106 @@ export const UserDetails = () => {
 			localStorage.setItem('user', JSON.stringify(data));
 			// Change state
 			dispatch({ type: 'UPDATE', payload: data });
-			// Navigate to profile
+			setTimeout(() => {
+				// Navigate to profile
 			navigate(`/profile/${data.username}`);
+			}, 2000)
+			// Success message
+			toast.success('Profile updated', {
+				position: "top-right",
+				autoClose: 1500,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: false,
+				progress: undefined,
+				theme: "dark",
+			});
 		} catch (error) {
-			console.log(error);
+			// Error message
+			toast.error(error, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "dark",
+            });
 		}
 	};
 
 	return (
-		<div className="details">
-			<div className="container">
-				<h1>User Details</h1>
-				{/* User details */}
-				<div className="user_details">
-					<div className="form_container">
-						<div className="input_container">
-							<label htmlFor="username">Username</label>
-							<input
-								type="text"
-								placeholder={userData.username}
-								id="username"
-								name="username"
-								value={profileData.username}
-								onChange={handleFormChange}
-								readOnly={edit ? false : true}
-							/>
+		<>
+			<div className="details">
+				<div className="container">
+					<h1>User Details</h1>
+					{/* User details */}
+					<div className="user_details">
+						<div className="form_container">
+							<div className="input_container">
+								<label htmlFor="username">Username</label>
+								<input
+									type="text"
+									placeholder={userData.username}
+									id="username"
+									name="username"
+									value={profileData.username}
+									onChange={handleFormChange}
+									readOnly={edit ? false : true}
+								/>
+							</div>
+							<div className="input_container">
+								<label htmlFor="email">Email</label>
+								<input
+									type="email"
+									placeholder={userData.email}
+									id="email"
+									name="email"
+									value={profileData.email}
+									onChange={handleFormChange}
+									readOnly={edit ? false : true}
+								/>
+							</div>
 						</div>
-						<div className="input_container">
-							<label htmlFor="email">Email</label>
-							<input
-								type="email"
-								placeholder={userData.email}
-								id="email"
-								name="email"
-								value={profileData.email}
-								onChange={handleFormChange}
-								readOnly={edit ? false : true}
-							/>
-						</div>
+						<button onClick={editUserData}>
+							{edit ? (
+								<>
+									<FaRegSave /> Save
+								</>
+							) : (
+								<>
+									<FaEdit /> Edit
+								</>
+							)}
+						</button>
 					</div>
-					<button onClick={() => setEdit((prevState) => !prevState)}>
-						{edit ? (
-							<>
-								<FaRegSave /> Save
-							</>
-						) : (
-							<>
-								<FaEdit /> Edit
-							</>
-						)}
-					</button>
-				</div>
-				{/* User image */}
-				<ProfileImage handleImageChange={handleImageChange} file={file} user={user} fileImage={fileImage} />
+					{/* User image */}
+					<ProfileImage handleImageChange={handleImageChange} file={file} user={user} fileImage={fileImage} />
 
-				<div className="btns">
-					<button id="back_btn" onClick={() => navigate(`/profile/${user.username}`)}>
-						Back
-					</button>
-					<button id="submit_btn" onClick={updateProfile}>
-						Submit
-					</button>
+					<div className="btns">
+						<button id="back_btn" onClick={() => navigate(`/profile/${user.username}`)}>
+							Back
+						</button>
+						<button id="submit_btn" onClick={updateProfile} disabled={isDisabled}>
+							Submit
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+			<ToastContainer
+				position="top-right"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable={false}
+				pauseOnHover
+				theme="dark"
+			/>
+		</>
 	);
 };
